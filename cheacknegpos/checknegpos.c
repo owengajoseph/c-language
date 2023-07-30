af3db02e3fc28b86723b2b798d8c1f6b3708ad80
owengajoseph <owengajoseph54@gmail.com>
2023-07-30 12:53:05 +0300

    original unmodified file

---
+++ cheacknegpos/checknegpos.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int arr[]={1,-2,5};
+    int store=0;
+    int store2=0;
+    int i;
+
+
+
+    for( i=0; i<sizeof(arr)/sizeof(int); i++){
+        if(arr[i]>0) {
+            store+=1;
+        }
+
+            else
+            store2+=1;      
+        }
+
+
+        if(store==2&&store2==1){
+            printf("yes");
+        }
+           
+        
+    
+}
