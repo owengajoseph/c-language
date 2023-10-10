304e063c456b22e72b11ceb3f24b7418312b4aa3
owenga joseph <owengajoseph54@gmail.com>
2023-10-10 11:56:07 +0300

    few changes

--- LEARNINGC/checknegpos.c
+++ LEARNINGC/checknegpos.c
@@ -1,29 +1,27 @@
-#include<stdio.h>
-// c program to check 
+#include <stdio.h>
+// c program to check
 // one negative num and more than on pos num
 
-int main(){
-    int arr[]={1,-2,5};
-    int store=0;
-    int store2=0;
+int main()
+{
+    int arr[] = {1, -2, 5};
+    int store = 0;
+    int store2 = 0;
     int i;
 
-
-
-    for( i=0; i<sizeof(arr)/sizeof(int); i++){
-        if(arr[i]>0) {
-            store+=1;
+    for (i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        if (arr[i] > 0)
+        {
+            store += 1;
         }
 
-            else
-            store2+=1;      
-        }
+        else
+            store2 += 1;
+    }
 
-
-        if(store==2&&store2==1){
-            printf("yes");
-        }
-           
-        
-    
+    if (store == 2 && store2 == 1)
+    {
+        printf("yes");
+    }
 }
--- LEARNINGC/dominate.c
+++ LEARNINGC/dominate.c
@@ -1,31 +1,27 @@
-#include<stdio.h>
-// checks the dominatar value in an arry
+#include <stdio.h>
+// checks the dominator value in an arry
 
+int dominator(int arry[], int s)
+{
 
-int dominator(int arry[], int s){
-
-    int num=0;
+    int num = 0;
     arry[0];
 
-    for( int i=0; i<=s; i++){
-        
-        if(arry[0]==arry[i]){
-            printf("%d",num=num+1);
-            arry[0]=arry[i];
+    for (int i = 0; i <= s; i++)
+    {
+
+        if (arry[0] == arry[i])
+        {
+            printf("%d", num = num + 1);
+            arry[0] = arry[i];
         }
-    
-        
-                  
-
     }
+}
 
+int main()
+{
 
-} 
-
-
-int main (){
-
-    int arr[]={3,4,3,2,3,1,3};
-    int size= (sizeof(arr)/sizeof(int));
+    int arr[] = {3, 4, 3, 2, 3, 1, 3};
+    int size = (sizeof(arr) / sizeof(int));
     dominator(arr, size);
 }
